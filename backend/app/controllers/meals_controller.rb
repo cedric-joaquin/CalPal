95ec563a2f218a2b78329b1b3ce6238a0b5fbd8a
cedric-joaquin <joaquin.cedric@gmail.com>
2020-12-13 12:49:29 -0800

    add actions

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -5,8 +5,19 @@ class MealsController < ApplicationController
     end
 
     def create
-        binding.pry
         meal = Meal.create(name: params[:name], calories: params[:calories], day_id: params[:day_id])
         render json: meal
     end
+
+    def update
+        meal = Meal.find_by(id: params[:id])
+        meal.update(name: params[:name], calories: params[:calories])
+        render json: meal
+    end
+
+    def destroy
+        meal = Meal.find_by(id: params[:id])
+        meal.destroy
+    end
+
 end
