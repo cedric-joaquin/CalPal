1ba9a946bb99b6b234cb9458b91f0699640f8fd9
cedric-joaquin <joaquin.cedric@gmail.com>
2020-12-13 12:48:52 -0800

    edit meal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -125,7 +125,7 @@ class Meal {
         })
         .then(resp => resp.json())
         .then(meal => {
-            this.newMeal(meal);
+            this.renderMeal(meal);
         });
     }
 
@@ -145,19 +145,88 @@ class Meal {
         })
     }
 
-    static newMeal(meal) {
-        let ul = document.querySelector('div.day ul');
-        let li = document.createElement("li");
+    static editMeal(meal) {
+        return fetch(`http://localhost:3000/meals/${meal.id.value}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                id: meal.id.value,
+                name: meal.name.value,
+                calories: meal.calories.value
+            })
+        })
+            .then(resp => resp.json())
+            .then(meal => {
+                this.renderMeal(meal)
+            })
+    }
 
+    static renderEditor(meal) {
+        let li = document.getElementById(`meal-${meal.id}`);
+        li.innerHTML = ''
+
+        let form = document.createElement('form');
+        form.className = `edit-meal-${meal.id}`
+
+        let editName = document.createElement('input');
+        editName.setAttribute('type', 'text');
+        editName.setAttribute('name', 'name');
+        editName.setAttribute('value', `${meal.name}`);
+
+        let editCalories = document.createElement('input');
+        editCalories.setAttribute('type', 'number');
+        editCalories.setAttribute('name', 'calories');
+        editCalories.setAttribute('placeholder', `${meal.calories} Calories`)
+        editCalories.setAttribute('value', `${meal.calories}`);
+
+        let mealId = document.createElement('input');
+        mealId.setAttribute('type', 'hidden');
+        mealId.setAttribute('name', 'id');
+        mealId.setAttribute('value', `${meal.id}`);
+
+        let editBtn = document.createElement('input');
+        editBtn.setAttribute('type', 'submit');
+        editBtn.setAttribute('value', 'Edit Meal');
+
+        form.appendChild(editName);
+        form.appendChild(editCalories);
+        form.appendChild(mealId);
+        form.appendChild(editBtn);
+
+        li.appendChild(form);
+
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            Meal.editMeal(e.target);
+        })
+    }
+
+    static renderMeal(meal) {
+        let li = document.createElement("li");
+        let mealElement = document.getElementById(`meal-${meal.id}`);
+        let editBtn = document.createElement('button');
         let deleteBtn = document.createElement('button');
+        editBtn.innerText = 'Edit Meal'
         deleteBtn.innerText = 'Delete Meal'
-        
         
         li.id = `meal-${meal.id}`
         li.innerText = `${meal.name} - ${meal.calories} Calories `;
+        
+        li.appendChild(editBtn);
         li.appendChild(deleteBtn);
+        editBtn.addEventListener('click', e => Meal.renderEditor(meal));
         deleteBtn.addEventListener('click', e => Meal.deleteMeal(meal));
-        ul.appendChild(li);
+        
+        if (mealElement) {
+            mealElement.innerText = ""
+            mealElement.appendChild(li);          
+        } else {
+            let ul = document.querySelector('div.day ul');
+            ul.appendChild(li);
+        }
     }
 }
 
@@ -173,7 +242,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 Meal.getMeals().then(meals => {
                     meals.forEach(meal => {
                         if (meal.day_id == day.id) {
-                            Meal.newMeal(meal);
+                            Meal.renderMeal(meal);
                         }
                     })
                 })
