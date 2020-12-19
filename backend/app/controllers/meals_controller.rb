bad7408d028b1b0647c9633c0987c3caa7dfc1af
cedric-joaquin <joaquin.cedric@gmail.com>
2020-12-19 10:31:35 -0800

    bug fix

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -6,18 +6,21 @@ class MealsController < ApplicationController
 
     def create
         meal = Meal.create(name: params[:name], calories: params[:calories], day_id: params[:day_id])
+        meal.day.total
         render json: meal
     end
 
     def update
         meal = Meal.find_by(id: params[:id])
         meal.update(name: params[:name], calories: params[:calories])
+        meal.day.total
         render json: meal
     end
 
     def destroy
         meal = Meal.find_by(id: params[:id])
         meal.destroy
+        meal.day.total
     end
 
 end
